acfa14f61df0194723bf142cb26c67ba42fa2834
Nelli Gorlova <nelli7035@gmail.com>
2024-10-03 05:45:26 +0600

    Added functionality for the checkbox and styles for it

--- src/components/UserItem/UserItem.tsx
+++ src/components/UserItem/UserItem.tsx
@@ -12,7 +12,9 @@ const UserItem: React.FC<IUserItemProps> = ({user}) => {
         <div className="p-2 ">
           <h3 className="user-name d-inline px-2 fs-2">{user.name}</h3>
           <span className="px-2 fs-4">{user.roles}</span>
-          <span className="user-active">{user.active}</span>
+          <span className="user-active" style={{color: user.active ? 'green' : 'red'}}>
+             {user.active ? 'ON' : 'OFF'}
+          </span>
         </div>
         <p className="user-email">{user.email}</p>
       </div>

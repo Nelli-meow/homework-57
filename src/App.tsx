3ba99cea1b0743deda20e9e786bb21f506a3adf2
Nelli Gorlova <nelli7035@gmail.com>
2024-10-03 06:11:21 +0600

    EX-57 is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 import "./App.css";
+import UserForm from './components/UserForm/UserForm.tsx';
+import Users from './components/Users/Users.tsx';
+import { useState } from 'react';
+import { IUser } from './types';
 
-const App = () => <></>;
+const App = () => {
+
+  const [user, setUser] = useState<IUser[]>([]);
+
+  const addNewUser = (newUser: IUser) => {
+    setUser(prevState => [...prevState, newUser]);
+  }
+
+  return (
+    <div className="container row">
+      <UserForm addNewUser={addNewUser} />
+      <div className="row col-6">
+        <Users users={user}/>
+      </div>
+    </div>
+    );
+
+}
+
 
 export default App;
